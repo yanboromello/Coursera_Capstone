8134cd176803304d9ed4eef25991e624f9a8bdd1
Boromello <Yan.Boromello1@BR.nestle.com>
2020-06-07 21:38:45 -0300

    Publish first vesion of the file

---
+++ IBM Data Science - Capstone - Final Course.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# # This Notebook will be used only for the IBM Data Science course - Capstone Project - Final Course
+# ## Yan Boromello
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
