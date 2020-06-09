3c8a36705bb68591d3fa1cfaadcacea39f705958
Boromello <Yan.Boromello1@BR.nestle.com>
2020-06-09 20:19:47 -0300

    Version done on Jupyter

---
+++ IBM_Capstone_FinalCourse.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# # This notebook will be mainly used for the Coursera Capstone Project
+# ## Author: Yan Boromello
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course')
+
