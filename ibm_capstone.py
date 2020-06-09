b165b1096135c6ba4d3c40a88bd6ea00273acd32
Boromello <Yan.Boromello1@BR.nestle.com>
2020-06-09 19:52:47 -0300

    new version using vim

---
+++ ibm_capstone.py
@@ -0,0 +1,10 @@
+
+
+# This notebook will be mainly used for the Coursera Capstone Project
+## Author: Yan Boromello
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course')
+
